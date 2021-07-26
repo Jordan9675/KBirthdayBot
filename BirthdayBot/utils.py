4289413dd1bf4a36b2356b65a7aca01db59f4b7e
Jordan9675 <vuong.jordan@gmail.com>
2021-07-26 19:20:22 +0200

    Handling hashtag for soloist

--- BirthdayBot/utils.py
+++ BirthdayBot/utils.py
@@ -1,2 +1,5 @@
 def convert_expression_to_hashtag(expression: str) -> str:
-    return "#" + "".join(char for char in expression if char.isalnum())
+    if expression.strip():
+        return "#" + "".join(char for char in expression if char.isalnum())
+    else:
+        return ""
