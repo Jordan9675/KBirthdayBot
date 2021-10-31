e73b812ab6012a3d12267bad78c7d3489e3d88b5
Jordan9675 <jordan.vuong96@gmail.com>
2021-10-31 17:52:26 +0100

    Handling end of month birthdays

--- BirthdayBot/birthday.py
+++ BirthdayBot/birthday.py
@@ -18,12 +18,13 @@ def convert_response_to_soup(response: Response) -> BeautifulSoup:
     return BeautifulSoup(response.text, "html.parser")
 
 
-def locate_birthday_table(soup: BeautifulSoup) -> BeautifulSoup:
-    return soup.select_one("#table_1")
+def locate_birthday_tables(soup: BeautifulSoup) -> BeautifulSoup:
+    return soup.select("#table_3, #table_1")
 
 
-def locate_rows_from_birthday_table(table: BeautifulSoup) -> BeautifulSoup:
-    return table.select("tbody > tr")
+def locate_rows_from_birthday_tables(tables: BeautifulSoup) -> BeautifulSoup:
+    rows = [table.select("tbody > tr") for table in tables]
+    return rows[0] + rows[1]
 
 
 def get_column_values(row: BeautifulSoup) -> list:
@@ -59,8 +60,8 @@ def get_birthdays() -> list:
     """Retrieving data about upcoming idols' birthdays."""
     response = request_url(BIRTHDAY_URL)
     soup = convert_response_to_soup(response)
-    table = locate_birthday_table(soup)
-    rows = locate_rows_from_birthday_table(table)
+    tables = locate_birthday_tables(soup)
+    rows = locate_rows_from_birthday_tables(tables)
     columns_values = [get_column_values(row) for row in rows]
     return [parse_column_values(
         columns_value) for columns_value in columns_values]
